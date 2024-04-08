7239f034d6e99371074737a0100a83abc48e3100
Roshan Bhatia <prakashbhatia1970@gmail.com>
2024-04-08 13:14:55 +0530

    migrations added

--- shop/models.py
+++ shop/models.py
@@ -83,7 +83,7 @@ class User(AbstractBaseUser):
         unique=True,
     )
     name = models.CharField(max_length=255)
-    tc = models.BooleanField(default=False)
+    tc = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
